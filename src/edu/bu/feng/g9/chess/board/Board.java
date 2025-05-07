39a406aa015060b45f8166640b9ce4adf46bb1ee
mohamed-abouedrise <mohamedabouedrise@gmail.com>
2025-05-08 01:07:10 +0300

    update

--- src/edu/bu/feng/g9/chess/board/Board.java
+++ src/edu/bu/feng/g9/chess/board/Board.java
@@ -1,11 +1,5 @@
 package edu.bu.feng.g9.chess.board;
 
 public class Board {
-    
-    
-    public static void main(String[] args) {
-        System.out.println("i am an eng,bro");
-    }
-    
-    
+
 }
