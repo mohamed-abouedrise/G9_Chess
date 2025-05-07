d2623a7ba5d0f3846a37aa455f3c2dc205f76b16
AboodMatter <engabdelrahmanmatter@gmail.com>
2025-05-08 01:04:26 +0300

    Update Board.java

--- src/edu/bu/feng/g9/chess/board/Board.java
+++ src/edu/bu/feng/g9/chess/board/Board.java
@@ -1,4 +1,11 @@
 package edu.bu.feng.g9.chess.board;
 
 public class Board {
+    
+    
+    public static void main(String[] args) {
+        System.out.println("i am an eng,bro");
+    }
+    
+    
 }
