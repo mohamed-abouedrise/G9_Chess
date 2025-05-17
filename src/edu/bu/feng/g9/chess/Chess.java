943b6dd6b6da8dbca7ff7bec23e34e3f28705934
mohamed-abouedrise <mohamedabouedrise@gmail.com>
2025-05-18 01:40:02 +0300

    linked gui with logic

---
+++ src/edu/bu/feng/g9/chess/Chess.java
@@ -0,0 +1,15 @@
+
+package edu.bu.feng.g9.chess;
+
+
+import edu.bu.feng.g9.chess.game.frame;
+
+public class Chess {
+
+    public static void main(String[] args) {
+        
+        new frame();
+
+    }
+    
+}
