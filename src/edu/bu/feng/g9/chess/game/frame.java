b5fc0ac9b62f425b7973bd8f0b24d5c561b5365c
AboodMatter <engabdelrahmanmatter@gmail.com>
2025-05-19 01:40:23 +0300

    TIMET GUI

--- src/edu/bu/feng/g9/chess/game/frame.java
+++ src/edu/bu/feng/g9/chess/game/frame.java
@@ -1,18 +1,28 @@
 package edu.bu.feng.g9.chess.game;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class frame extends JFrame{
     
     panel panel;
-
+    JLabel clock,clock2;
     public frame(){
+        clock = new clock();
+        clock2 = new clock2();
         panel = new panel();
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.add(panel);
-        this.pack();
-        this.setLocationRelativeTo(null);        
         this.setLayout(null);
+        panel.setLocation(0, 50);
+                this.add(panel);
+                
+
+        this.add(clock);
+        this.add(clock2);
+        
+        this.setSize(2000, 750);
+        
+        this.setLocationRelativeTo(null);        
         this.setVisible(true);
     }
     
