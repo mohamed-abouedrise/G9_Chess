4665d95ccaf51fd9a0e1268cf1c5827c17596408
AboodMatter <engabdelrahmanmatter@gmail.com>
2025-05-19 11:27:53 +0300

    frame (last update)

--- src/edu/bu/feng/g9/chess/game/frame.java
+++ src/edu/bu/feng/g9/chess/game/frame.java
@@ -7,16 +7,26 @@ public class frame extends JFrame{
     
     panel panel;
     JLabel clock,clock2;
+        button1 newGameButton;
+    button2 pauseButton;
+    buttonsitting sittingButton;
     public frame(){
         clock = new clock();
         clock2 = new clock2();
         panel = new panel();
+        pauseButton=new button2();
+        sittingButton=new buttonsitting();
+        newGameButton=new button1();
+        
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setLayout(null);
         panel.setLocation(0, 50);
         this.add(panel);
         this.add(clock);
         this.add(clock2);
+        this.add(newGameButton);
+        this.add(pauseButton);
+        this.add(sittingButton);
         this.setSize(2000, 750);
         this.setLocationRelativeTo(null);        
         this.setVisible(true);
